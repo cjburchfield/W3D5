2be80255d4b7749bf2949529bb19bec004428afd
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-27 13:58:59 -0500

    Setting up node class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,35 @@
 class PolyTreeNode
+    def initialize (value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+        @value
+    end
 
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+
+    def parent=(new_parent)
+        if !self.parent.nil?
+            self.parent.children.remove(self)
+        else
+            @parent = new_parent 
+            new_parent.children << self
+
+
+        parent.children.each do |child|
+            if self == child
+
+        @parent = parent
+        parent.children << self 
+    end
+
+
+        
 end
