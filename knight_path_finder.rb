c43f57c6117955af8e1d4437c082fc13d0fda308
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-27 16:40:50 -0500

    move work

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -14,8 +14,25 @@ class KnightPathFinder
         self.root_node(pos)
     end
 
+
+    def self.valid?(pos)
+        x, y = pos
+        return false if x < 0 && x > 7
+        return false if y < 0 && y > 7
+    end
+
     def self.valid_moves(pos)
-        
+        x, y = pos
+        results = []
+
+       results << [x - 2, y + 1] if KnightPathFinder.valid?([x - 2, y + 1])
+        KnightPathFinder.valid?([x - 1, y + 2])
+        KnightPathFinder.valid?([x + 1, y + 2])
+        KnightPathFinder.valid?([x + 2, y + 1])
+        KnightPathFinder.valid?([x + 2, y - 1])
+        KnightPathFinder.valid?([x + 1, y - 2])
+        KnightPathFinder.valid?([x - 1, y - 2])
+        KnightPathFinder.valid?([x - 2, y - 1])
     end
 
 
