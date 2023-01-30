124174edcca26490bb30f9bf970e0a11ce8fcc87
Steven Paalz <stevenpaalz@gmail.com>
2023-01-30 11:27:00 -0500

    build move tree

--- knight_path_finder.rb
+++ knight_path_finder.rb
@@ -1,24 +1,32 @@
-require "/skeleton/lib/00_tree_node.rb"
+require_relative "./skeleton/lib/00_tree_node.rb"
 
 class KnightPathFinder
     def self.root_node(pos)
-        @initial_pos = PolyTreeNode.new(pos)
+        PolyTreeNode.new(pos)
     end
 
     def initialize(starting_pos)
-        build_move_tree(starting_pos)
-        @considered_positions = [starting_pos]
+        @initial_pos = KnightPathFinder.root_node(starting_pos)
+        @considered_positions = [@initial_pos.value]
+        self.build_move_tree
     end
 
-    def build_move_tree(pos)
-        self.root_node(pos)
+    # this is the iteration
+    def build_move_tree
+        queue = @considered_positions.dup
+        until @considered_positions.length == 64
+            new_positions = self.new_move_positions(queue.shift())
+            queue += new_positions
+        end
+        @considered_positions
     end
 
 
     def self.valid?(pos)
         x, y = pos
-        return false if x < 0 && x > 7
-        return false if y < 0 && y > 7
+        return false if x < 0 || x > 7
+        return false if y < 0 || y > 7
+        true
     end
 
     def self.valid_moves(pos)
@@ -37,18 +45,19 @@ class KnightPathFinder
         results
     end
 
-    # NOT YET TESTED!!!!!!
+    # this is a step
     def new_move_positions(pos)
-        new_positions = []
-        @considered_positions
 
-        KnightPathFinder.valid_moves(pos).each do |pos|
-            if !@considered_positions.include?(pos)
-                new_positions << pos 
-                @considered_positions << pos
+        new_positions = []
+        KnightPathFinder.valid_moves(pos).each do |new_pos|
+            if !@considered_positions.include?(new_pos)
+                new_positions << new_pos
+                @considered_positions << new_pos
             end
         end
         new_positions
     end
     
 end
+
+tst = KnightPathFinder.new([4,5])
